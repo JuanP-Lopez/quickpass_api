a7c64160f39ef12fd4d42879fc022ac8377a4745
Juan Lopez <137343283+JuanP-Lopez@users.noreply.github.com>
2026-06-29 19:43:57 -0600

    Subica a Railway

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("Frontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000","https://quickpass-production.up.railway.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
